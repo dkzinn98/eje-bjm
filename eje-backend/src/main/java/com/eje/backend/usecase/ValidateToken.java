33cfd75b0d193b56cf144fdeaecf264e00ac1511
vinicius-rocha <viniciusrocha@xipptech.com.br>
2025-06-13 22:54:14 -0300

    feat: Criado caso de uso ValidateToken

---
+++ eje-backend/src/main/java/com/eje/backend/usecase/ValidateToken.java
@@ -0,0 +1,31 @@
+package com.eje.backend.usecase;
+
+import com.eje.backend.exceptions.InvalidTokenException;
+import com.eje.backend.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidateToken implements UseCase<ValidateToken.Input, Boolean> {
+
+    private final JwtService jwtService;
+
+    @Override
+    public Boolean execute(Input input) {
+        validateToken(input.token());
+        return true;
+    }
+
+    private void validateToken(String token) {
+        var username = jwtService.extractUsername(token);
+        var isValidToken = jwtService.validateToken(token, username);
+        if (isValidToken) return;
+        throw new InvalidTokenException();
+    }
+
+    public record Input(String token) {
+    }
+}
